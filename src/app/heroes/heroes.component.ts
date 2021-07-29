622d3e1e7a97cbb7056044874653733abbaede97
Kadievka Salcedo <ksalcedo@EMPCCS.LOCAL>
2021-07-29 14:39:35 -0400

    two-way binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,7 +3,7 @@ import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes', // the component's CSS element selector
-  templateUrl: './heroes.component.html', // the locartion of the component's template file
+  templateUrl: './heroes.component.html', // the location of the component's template file
   styleUrls: ['./heroes.component.css'] // the location of the component's private CSS styles
 })
 export class HeroesComponent implements OnInit {
